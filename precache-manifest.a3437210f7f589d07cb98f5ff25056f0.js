bbc24384862240c546f0c813fdf358dde6a270bf
zhengge-lang <794303044@qq.com>
2021-03-18 15:46:09 +0800

    deploy

---
+++ precache-manifest.a3437210f7f589d07cb98f5ff25056f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08432e00d95765beb6c",
+    "url": "/morney-3-website/css/app.9448b582.css"
+  },
+  {
+    "revision": "942c3e798f06e3597af8fbadd0563b47",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "f08432e00d95765beb6c",
+    "url": "/morney-3-website/js/app.77594f48.js"
+  },
+  {
+    "revision": "081fabe54d57e70498df",
+    "url": "/morney-3-website/js/chunk-vendors.8fd3cd7b.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
